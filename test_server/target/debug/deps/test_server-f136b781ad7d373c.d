6d6536f787769d27bb8b3871382d5f4f6458be6f
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2024-07-16 14:10:14 +0200

    chore: remove inner targe

--- test_server/target/debug/deps/test_server-f136b781ad7d373c.d
+++
@@ -1,5 +0,0 @@
-/home/silen/proj/htcp-server/test_server/target/debug/deps/test_server-f136b781ad7d373c: src/main.rs
-
-/home/silen/proj/htcp-server/test_server/target/debug/deps/test_server-f136b781ad7d373c.d: src/main.rs
-
-src/main.rs:
--- test_server/target/debug/test-server.d
+++
@@ -1,1 +0,0 @@
-/home/silen/proj/htcp-server/test_server/target/debug/test-server: /home/silen/proj/htcp-server/test_server/src/main.rs
